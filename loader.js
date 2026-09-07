df59a26698e8220dd7b84c95aa78a148177f6a90
Xal'Morak <margadmargad727@gmail.com>
2026-09-08 01:44:09 +0800

    Remove unused loader.js

--- loader.js
+++
@@ -1,9 +0,0 @@
-/* load both halves */
-(function(){
-  function load(src){return fetch(src).then(function(r){return r.text();});}
-  Promise.all([load('./bundle_a.js'),load('./bundle_b.js')]).then(function(parts){
-    var s=document.createElement('script');
-    s.textContent=parts[0]+parts[1];
-    document.body.appendChild(s);
-  }).catch(function(e){console.error(e);alert('Kod achaalah aldaa');});
-})();
